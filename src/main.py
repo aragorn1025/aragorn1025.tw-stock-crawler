9ded3704cc22c231b45a43ddcc3e5dd9ca71334a
aragorn1025 <tzjie.dai@gmail.com>
2025-08-31 11:06:07 -0700

    style: run lint

--- src/main.py
+++ src/main.py
@@ -112,6 +112,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    all_stock_data = crawl_all_data(stock_numbers=args.stock_numbers.split(","), year=args.year)
+    all_stock_data = crawl_all_data(
+        stock_numbers=args.stock_numbers.split(","),
+        year=args.year,
+    )
     result = convert_to_data_frame(all_stock_data)
     result.to_csv(args.output, encoding="utf-8")
